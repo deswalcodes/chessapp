dd278c84429f42b8f080c036c83b43a9e58eaaaf
deswalcodes <21cs2014@rgipt.ac.in>
2025-05-07 18:02:30 +0530

    working on backend

---
+++ backend1/src/Game.ts
@@ -0,0 +1,28 @@
+import { Chess } from "chess.js";
+import { WebSocket } from "ws";
+
+
+export class Game {
+    public player1 : WebSocket;
+    public player2 : WebSocket;
+    
+    private board : Chess;
+    private startTime : Date;
+
+    constructor(player1:WebSocket,player2:WebSocket){
+        this.player1 = player1;
+        this.player2 = player2;
+        this.board = new Chess();
+        
+        this.startTime = new Date();
+    }
+    addMove(socket:WebSocket,move:{
+        from : string,
+        to : string
+    }){
+        
+
+
+    }
+
+}
